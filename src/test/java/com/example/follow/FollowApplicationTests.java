b5ef82e7aa2f39177973392b3aa2c9460f2a9428
tzz2015 <1374329135@qq.com>
2023-07-02 16:09:04 +0800

    初始化

---
+++ src/test/java/com/example/follow/FollowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.follow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
